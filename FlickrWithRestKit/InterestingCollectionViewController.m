a20695e758878c4df1606e60d72891c909bb5f6a
Michael Ting <realq86@gmail.com>
2015-10-17 15:50:55 -0700

    Create delegate responds from user's selection of Collection View Cell. - Store an array of Photo objects to know which photo is chosen for the next view controller. - Set the chosen photo to the next view controller - Prepare for segue.
    Changed UX.
    - Content in this view will not refresh on every viewWillAppear.

--- FlickrWithRestKit/InterestingCollectionViewController.m
+++ FlickrWithRestKit/InterestingCollectionViewController.m
@@ -8,10 +8,13 @@
 
 #import "InterestingCollectionViewController.h"
 #import "ParentCollectionViewCell.h"
-
+#import "DetailViewController.h"
 
 
 @interface InterestingCollectionViewController ()
+@property (nonatomic) NSArray *photoObjectsArray;
+@property (nonatomic) Photo *selectedPhotoObject;
+
 
 @end
 
@@ -24,40 +27,54 @@
     //Set UI of Navigationbar.
     self.navigationItem.title = @"Interesting";
     self.navigationController.navigationBar.backgroundColor = [UIColor blueColor];
-
-    
-}
-
-- (void)viewWillAppear:(BOOL)animated{
     
     //Instanciate FlickrServer instance, and load 20 photos at the designated photoSize
     FlickrServer *flickrServer = [FlickrServer sharedInstance];
     [flickrServer setValidPageSize:@"20"];
     [flickrServer setValidPageIndex:self.pageIndex];
+    
+    //Call Flickr API with method Interesting
     [flickrServer flickrInterestingnessGetListAtSize:self.photoSize withBlock:^(NSError *error, NSArray *photoObjectsArray)
-    {
-        [flickrServer downLoadPhotos:photoObjectsArray WithCompletionBlock:^(NSMutableDictionary *uiImageDictionary) {
-            self.uiImageDictionary = uiImageDictionary;
-            [self.collectionView reloadData];
-        }];
-    }];
-
+     {
+         //Store the downloaded Photo Object to notify the next ViewController.
+         self.photoObjectsArray = photoObjectsArray;
+         
+         //Use the result URL from method Interesting to fetch image data
+         [flickrServer downLoadPhotos:self.photoObjectsArray WithCompletionBlock:^(NSMutableDictionary *uiImageDictionary) {
+ 
+             //Set the designated data back for tableview.
+             self.uiImageDictionary = uiImageDictionary;
+             [self.collectionView reloadData];
+         }];
+     }];
 }
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+    
+    //User selected a cell determin which is selected and push to next ViewController
+    self.selectedPhotoObject = self.photoObjectsArray[indexPath.row];
+    [self performSegueWithIdentifier:@"showDetailViewSegueID" sender:nil];
+    
+}
 
-/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    
+    //Set the Photo Object to display at the next ViewController
+    DetailViewController *detailViewController = [segue destinationViewController];
+    detailViewController.photoObject = self.selectedPhotoObject;
+    
 }
-*/
+
+
 
 @end
--- FlickrWithRestKit/RecentCollectionViewController.m
+++ FlickrWithRestKit/RecentCollectionViewController.m
@@ -8,9 +8,14 @@
 
 #import "RecentCollectionViewController.h"
 #import "ParentCollectionViewCell.h"
-
+#import "DetailViewController.h"
+#import "Photo.h"
 
 @interface RecentCollectionViewController ()
+
+@property (nonatomic) Photo *selectedPhotoObject;
+@property (nonatomic) NSArray *photoObjectsArray;
+
 
 @end
 
@@ -23,22 +28,30 @@
     //Set UI of Navigationbar.
     self.navigationItem.title = @"getRecent";
     self.navigationController.navigationBar.backgroundColor = [UIColor redColor];
-
-}
-
-- (void)viewWillAppear:(BOOL)animated{
     
     //Instanciate FlickrServer instance, and load 20 photos at the designated photoSize
     FlickrServer *flickrServer = [FlickrServer sharedInstance];
     //[flickrServer setFlickrAPIKey:kCLIENTID];
     [flickrServer setValidPageSize:@"20"];
     [flickrServer setValidPageIndex:self.pageIndex];
+    
+    //Call Flickr API with method Recent
     [flickrServer flickrPhotosRecentAtSize:self.photoSize withBlock:^(NSError *error, NSArray *photoObjectsArray) {
-        [flickrServer downLoadPhotos:photoObjectsArray WithCompletionBlock:^(NSMutableDictionary *uiImageDictionary) {
+        
+        //Store the downloaded Photo Object to notify the next ViewController.
+        self.photoObjectsArray = photoObjectsArray;
+        
+        //Use the result URL from method Recent to fetch image data
+        [flickrServer downLoadPhotos:self.photoObjectsArray WithCompletionBlock:^(NSMutableDictionary *uiImageDictionary) {
+
+            //Set the designated data back for tableview.
             self.uiImageDictionary = uiImageDictionary;
             [self.collectionView reloadData];
         }];
-    }];}
+    }];
+
+}
+
 
 
 - (void)didReceiveMemoryWarning {
@@ -46,15 +59,25 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+    
+    //User selected a cell determin which is selected and push to next ViewController
+    self.selectedPhotoObject = self.photoObjectsArray[indexPath.row];
+    [self performSegueWithIdentifier:@"showDetailViewSegueID" sender:nil];
+    
+}
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+
+    //Set the Photo Object to display at the next ViewController
+    DetailViewController *detailViewController = [segue destinationViewController];
+    detailViewController.photoObject = self.selectedPhotoObject;
+    
 }
-*/
+
 
 @end
